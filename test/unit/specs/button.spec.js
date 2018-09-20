244568bb02950cdeb7eacac7ff04681df784b5e3
dzj <414603173@qq.com>
2018-09-20 20:08:50 +0800

    单元测试配置完成

---
+++ test/unit/specs/button.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Button from '../../../packages/button'
+
+describe('Button', () => {
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor().$mount()
+        expect(vm.type)
+            .to.be.equal('default')
+    })
+})
